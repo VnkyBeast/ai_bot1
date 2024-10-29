02001da578a700237e5eae7341d75315c8cc0fa0
VnkyBeast <146955019+VnkyBeast@users.noreply.github.com>
2024-10-30 04:17:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/api/chat', async (req, res) => {
 
     try {
         const fetch = (await import('node-fetch')).default;  // Dynamic import here
-         const response = await fetch('https://api-inference.huggingface.co/models/mistralai/Mistral-7B-v0.1',{
+         const response = await fetch('https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B',{
             method: 'POST',
             headers: {
                 'Authorization': `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
